154cb7f8650b776981543984cc4b1beb021045fd
Russell Gove <russellgove@gmail.com>
2017-05-29 18:47:54 -0400

    initialgrid

--- src/webparts/trTimeCard/components/TrTimeCard.tsx
+++ src/webparts/trTimeCard/components/TrTimeCard.tsx
@@ -22,13 +22,17 @@ import { IPersonaWithMenu } from 'office-ui-fabric-react/lib/components/pickers/
 import { SPComponentLoader } from '@microsoft/sp-loader';
 import * as moment from 'moment';
 import * as _ from "lodash";
+import { TimeSpent } from "../dataModel";
 export default class TrTimeCard extends React.Component<ITrTimeCardProps, ITrTimeCardState> {
   constructor(props: ITrTimeCardProps) {
     debugger;
     super(props);
     this.state = props.initialState;
     this.setState(this.state);
-
+    this.getDisplayTRs = this.getDisplayTRs.bind(this);
+  }
+  public getDisplayTRs(): Array<TimeSpent> {
+    return this.state.timeSpents;
   }
 
   public render(): React.ReactElement<ITrTimeCardProps> {
@@ -36,8 +40,21 @@ export default class TrTimeCard extends React.Component<ITrTimeCardProps, ITrTim
     return (
       <div>
         <Label>Time spent by Technical Specialist {this.props.userName} for the week ending {this.state.weekEndingDate.toDateString()}   </Label>
-       
+        <Label>(if you do not see a TR you are working on displayed here, please ask you adminstrator to assign it to you, or to reopen it) </Label>
+        
+        <DetailsList
+          layoutMode={DetailsListLayoutMode.fixedColumns}
+          selectionMode={SelectionMode.none}
+          items={this.getDisplayTRs()}
+          setKey="id"
+          columns={[
+            { key: "Id", name: "Id", fieldName: "Id", minWidth: 20, maxWidth: 200 },
+            { key: "TR", name: "TR", fieldName: "TR", minWidth: 20, maxWidth: 200 },
+            { key: "HoursSpent", name: "HoursSpent", fieldName: "HoursSpent", minWidth: 200 }
+          ]}
+        />
       </div>
+
     );
   }
 }
